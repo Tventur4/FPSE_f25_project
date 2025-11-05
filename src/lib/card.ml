fad44c084a7bd565cb6db0ac7e5449ae9956952d
Tventur4 <Tventur4@jhu.edu>
2025-11-05 16:15:11 -0500

    added deck.mli + ml files, added function declarations along with documentation

--- src/lib/card.ml
+++ src/lib/card.ml
@@ -3,6 +3,9 @@ open Core
 [@@@ocaml.warning "-27"]
 [@@@ocaml.warning "-32"]
 
+(*
+  Placeholder for unimplemented functions.
+*)
 let unimplemented () =
   failwith "unimplemented"
 
@@ -12,18 +15,53 @@ type rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack 
 
 type t = { suit : suit; rank : rank} [@@deriving sexp, compare, equal]
 
+(*
+  [suit_to_int s] is the numeric representation of [s], with [Clubs] -> 0, [Diamonds] -> 1, [Hearts] -> 2, 
+  and [Spades] -> 3.
+
+  @param s Suit.
+  @return The numeric representation of [s].
+*)
 let suit_to_int (s : suit) : int =
   unimplemented ()
 
+(*
+  [rank_to_int r] is the numeric representation of [r], where each rank is assigned a value from 0-12 corresponding 
+  to their rank-value with respect to poker (i.e. [rank_to_int 2] = 0 and [rank_to_int Ace] = 12).
+
+  @param r Rank.
+  @return The numeric representation of [r].
+*)
 let rank_to_int (r : rank) : int =
   unimplemented ()
 
+(*
+  [card_to_int card] is the numeric representation of [card], where the card is assigned a value from 0-51
+  corresponding to its index in a 52-card deck sorted in ascending order by rank and then by suit.
+
+  @param card Input card to convert.
+  @return The numeric representation of [card].
+*)
 let card_to_int (card : t) : int =
   unimplemented ()
 
+(*
+  [int_to_card index] converts an index (i.e. a number between 0 and 51 inclusive) into a card with appropriate 
+  suit and index. This is an inverse function to [card_to_int].
+
+  @param index The integer index of the desired card.
+  @return The card correspdoning to [index].
+  @throws invalid_arg if [index] < 0 or [index] > 51.
+*)
 let int_to_card (index : int) : t =
   unimplemented ()
 
+(*
+  [to_string card] gets the string representation of [card], with the format of "[rank] of [suit]."
+  
+  @param card The input card.
+  @return The string representation of [card].
+*)
 let to_string (card : t) : string =
   unimplemented ()
 
---
+++ src/lib/deck.mli
@@ -0,0 +1,30 @@
+(*
+  The Deck module defines a concrete type for a standard deck of 52 playing cards as well as functions to construct, 
+  manipulate, and print them.
+*)
+
+module C = Card
+
+type t [@@deriving sexp]
+(** [t] is a serializable type to represent a deck of playing cards.
+    Note that the "top" of the deck refers to the card at index 0. *)
+
+val get_deck : unit -> t
+(** [get_deck] is a standard deck of 52 playing cards sorted in ascending order by rank and then by suit. *)
+
+val shuffle : t -> t
+(** [shuffle deck] shuffles [deck], randomly rearranging the card. *)
+
+val num_cards : t -> int
+(** [num_card deck] is the number of cards currently in [deck]. *)
+
+val draw_card : t -> C.t
+(** [draw_card deck] draws a single card from the top of [deck]. *)
+
+val draw_cards : t -> int -> C.t list 
+(** [draw_cards deck n] draw [n] cards from the top of [deck]. *)
+
+val burn_card : t -> unit
+(** [burn_card deck] takes a card from the top of [deck] and discards it. *)
+
+
