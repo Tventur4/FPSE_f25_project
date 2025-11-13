80c8ddbac04209fa01b8b5f640ee58d2d61f822d
Timothy <Tventur4@jhu.edu>
2025-11-12 19:58:03 -0500

    added starter .mli files for game and bot

---
+++ src/lib/bot.mli
@@ -0,0 +1,7 @@
+(*
+  The Bot module defines an abstract type representing an driving algorithm and behavior of a computer player in a game of poker.
+*)
+
+module C = Card
+module P = Player
+module G = Game
---
+++ src/lib/game.mli
@@ -0,0 +1,8 @@
+(*
+  The Game module defines a concrete type to represent a game of poker as well as all of the necessary functions to 
+  interact with the game.
+*)
+
+module C = Card
+module D = Deck
+module P = Player
