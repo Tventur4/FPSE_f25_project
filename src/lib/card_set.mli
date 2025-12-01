8c5d2ae262a7e6a01dbe6387f605779b0bbbfa02
Tobi Ijiyemi <TobiIjiyemi@gmail.com>
2025-12-01 13:30:14 -0500

    implementing card_set.mli

--- src/lib/card_set.mli
+++ src/lib/card_set.mli
@@ -4,3 +4,17 @@
 *)
 
 type t [@@deriving sexp]
+
+(* returns best possible 5-card hand constructed from 7 available hands. takes two hole cards and
+community cards as parameters*)
+val of_7_cards : C.t -> C.t -> C.t list -> t
+
+(* return comparable integer score used to rank hands*)
+val value_of_hand : t -> int
+
+(*see if one hand beats another and return integer representing clash.
+  1 if hand1 beats hand2, -1 if h2 beats h1, and 0 if tie*)
+val compare : t -> t -> int
+
+(*prints nicer hand description*)
+val to_string : t -> string
